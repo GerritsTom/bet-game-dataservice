29e0600c5341869c6cae50c14c7f36cab9b24f08
Client Administrator <adm.t.gerrits@christ.local>
2018-02-11 11:08:03 +0100

    change to bcrypt package again

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt-nodejs');
+const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
@@ -47,6 +47,9 @@ router.post('/signup', (req, res, next) => {
 
 router.post('/login', (req, res, next) => {
     // Check if user exists
+    console.log('process.env.JWT_KEY');
+    console.log(process.env.JWT_KEY);
+    process.env.JWT_KEY = 'secret';
     User.find({email: req.body.email})
     .exec()
     .then(user => {
@@ -65,7 +68,7 @@ router.post('/login', (req, res, next) => {
                 const token = jwt.sign({
                     email: user[0].email,   
                     userId: user[0]._id
-                },  process.env.JWT_KEY, {
+                },  'secret', {
                     expiresIn: "1h"    
                 });
                 return res.status(200).json({
